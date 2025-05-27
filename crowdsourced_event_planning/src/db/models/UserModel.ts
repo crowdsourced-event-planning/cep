43190732e9c83db6c61f9e17af1c7e4ec86429f6
Andika Rahmadi Saputra <andikars811@gmail.com>
2025-05-27 22:58:37 +0700

    feat(User): update user model

--- crowdsourced_event_planning/src/db/models/UserModel.ts
+++ crowdsourced_event_planning/src/db/models/UserModel.ts
@@ -1,10 +1,11 @@
-import mongoose from "mongoose";
-import User, { IUser } from "./User";
+import mongoose, { model } from "mongoose";
 import { hashPassword, comparePassword } from "@/lib/bcrypt";
 import CustomError from "@/db/exceptions/CustomError";
 import { signToken } from "@/lib/jwt";
 import { z } from "zod";
 import { dbConnect } from "../config/mongoose";
+import { IUser, UserSchema } from "../schemas/user.schema";
+import { validateObjectId } from "../utils/validateObjectId";
 
 const userSchema = z.object({
   name: z.string().min(3),
@@ -26,11 +27,14 @@ interface ILoginResponse {
   access_token: string;
 }
 
+const User = mongoose.models.User || model<IUser>("User", UserSchema);
+
 export default class UserModel {
   static async getById(id: string): Promise<IUser | null> {
     await dbConnect();
 
-    if (!mongoose.Types.ObjectId.isValid(id)) return null;
+    validateObjectId(id, "User ID");
+
     const user = await User.findById<IUser>(id).lean<IUser>();
 
     return user;
@@ -73,7 +77,8 @@ export default class UserModel {
   static async update(id: string, data: Partial<IUser>): Promise<IUser | null> {
     await dbConnect();
 
-    if (!mongoose.Types.ObjectId.isValid(id)) return null;
+    validateObjectId(id, "User ID");
+
     return await User.findByIdAndUpdate<IUser>(id, data, {
       new: true,
     }).lean<IUser>();
@@ -82,8 +87,8 @@ export default class UserModel {
   static async delete(id: string): Promise<string> {
     await dbConnect();
 
-    if (!mongoose.Types.ObjectId.isValid(id))
-      throw new CustomError("Invalid ID", 400);
+    validateObjectId(id, "User ID");
+
     await User.findByIdAndDelete(id);
     return "User deleted";
   }
